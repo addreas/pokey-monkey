cf57470f2b9c11e180d2f5a20ad2ffaf1767e60a
Andreas Mårtensson <andreas@addem.se>
2023-02-23 21:57:27 +0100

    such design

--- app.tsx
+++ app.tsx
@@ -1,46 +1,86 @@
-import { PodList } from "https://deno.land/x/kubernetes_apis@v0.4.0/builtin/core@v1/structs.ts";
+import groupBy from "https://deno.land/x/denodash@0.1.3/src/collection/groupBy.ts";
+import {
+  Pod,
+  PodList,
+} from "https://deno.land/x/kubernetes_apis@v0.4.0/builtin/core@v1/structs.ts";
 
 export function App({ pods }: { pods: PodList }) {
-  // layout stolen from https://www.kindacode.com/article/tailwind-css-grid-examples/
   return (
-    <div tw="mx-auto container grid grid-cols-5">
-      <header tw="col-span-5 p-10 bg-amber-300">
-        <h1 tw="text-center text-2xl">Header</h1>
-      </header>
-      <aside tw="col-span-5 md:col-span-1 p-10 bg-gray-700">
-        <h1 tw="text-center text-2xl text-white">Menu</h1>
-      </aside>
-
-      <main tw="col-span-5 md:col-span-3 p-10 bg-blue-200">
-        <h1 tw="text-center text-2xl">Main Content</h1>
-        {pods.items.map(({ metadata, status }) => (
-          <div>
-            <form method="POST">
-              <input
-                type="hidden"
-                name="namespace"
-                value={metadata?.namespace as string}
-              />
-              <input
-                type="hidden"
-                name="name"
-                value={metadata?.name as string}
-              />
-              <button type="submit">
-                {metadata?.namespace}/{metadata?.name} ({status?.phase})
-              </button>
-            </form>
-          </div>
-        ))}
-      </main>
-
-      <aside tw="col-span-5 md:col-span-1 p-10 bg-rose-300">
-        <h1 tw="text-center text-2xl">Sidebar</h1>
-      </aside>
-
-      <footer tw="col-span-5 p-10 bg-green-300">
-        <h1 tw="text-center text-2xl">Footer</h1>
-      </footer>
+    <div tw="mx-auto container">
+        <GroupByNamespace pods={pods} />
     </div>
   );
+}
+
+function GroupByNamespace({ pods }: { pods: PodList }) {
+  const grouped = Object.entries(
+    groupBy((p) => p.metadata?.namespace, pods.items)
+  );
+  return (
+    <>
+      {grouped.map(([namespace, pods]) => (
+        <div tw="bg-emerald-400 rounded">
+          <h2 tw="text-center text-2xl col-span-3 md:col-span-5 p-2">
+            {namespace}
+          </h2>
+          <form
+            method="POST"
+            tw="grid grid-cols-3 md:grid-cols-5 bg-rose-200 p-2 gap-2 rounded"
+          >
+            <input type="hidden" name="namespace" value={namespace} />
+            <GroupByWorkload pods={pods} />
+          </form>
+        </div>
+      ))}
+    </>
+  );
+}
+
+function GroupByWorkload({ pods }: { pods: Pod[] }) {
+  const grouped = Object.entries(
+    groupBy(
+      (p) =>
+        p.metadata?.ownerReferences
+          ?.map((o) => `${o.kind}: ${o.name}`)
+          .join(", "),
+      pods
+    )
+  );
+  return (
+    <>
+      {grouped.map(([owner, pods]) => (
+        <>
+          <div tw="bg-fuchsia-600 rounded">
+            <h3 tw="text-center p-2">{owner}</h3>
+            {pods.map((p) => (
+              <div tw="flex m-2">
+                <button
+                  type="submit"
+                  name="name"
+                  value={p.metadata?.name as string}
+                  tw="flex-grow bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded py-4 "
+                >
+                  {p.status?.phase} {maybeSurround(trimPrefix(trimPrefix(trimPrefix(p.metadata?.name, p.metadata?.ownerReferences?.at(0)?.name), "-"), "0"), "()")}
+                </button>
+              </div>
+            ))}
+          </div>
+        </>
+      ))}
+    </>
+  );
+}
+
+function trimPrefix(str: string | null | undefined, prefix: string | null | undefined): string | null {
+  if(str && prefix && str.startsWith(prefix)) {
+    return str.substring(prefix.length)
+  }
+  return str ?? null
+}
+
+function maybeSurround(str: string | null, ends: string): string | null {
+  if (str && ends.length === 2) {
+    return `${ends[0]}${str}${ends[1]}`
+  }
+  return null
 }
